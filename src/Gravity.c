efc0ae8d4dfc48f63ce91139c6a03ecd4ab41bf1
Peter Hardy <peter@hardy.dropbear.id.au>
2013-11-07 13:35:05 +1100

    First conversion to SDK 2.0.

--- src/Gravity.c
+++ src/Gravity.c
@@ -1,40 +1,21 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
 
 #include "my_math.h"
 
-#include "config.h"
-
-#ifdef INVERTED
-#define FOREGROUND GColorBlack
-#define BACKGROUND GColorWhite
-#define APP_NAME "Gravity Inverted"
-#define MY_UUID { 0x7A, 0xAB, 0xF7, 0xB2, 0x0B, 0x49, 0x48, 0x61, 0xA2, 0x17, 0x28, 0x24, 0x8B, 0xEF, 0x43, 0x5C }
-#else
 #define FOREGROUND GColorWhite
 #define BACKGROUND GColorBlack
-#define APP_NAME "Gravity"
-#define MY_UUID { 0x7B, 0x4E, 0x89, 0x33, 0x1E, 0x3F, 0x4B, 0x57, 0xBE, 0x13, 0xD0, 0x7D, 0x76, 0xB6, 0x14, 0x17 }
-#endif
-
-PBL_APP_INFO(MY_UUID,
-             APP_NAME, "Kids, Inc.",
-             1, 2, /* App version */
-             DEFAULT_MENU_ICON,
-             APP_INFO_WATCH_FACE);
 
 #define DIAL_RADIUS 70
 #define V_OFFSET 25
 
 #define RADS 2*M_PI
 
-Window window;
+Window *root_window;
 
-Layer dial_layer, hour_layer, minute_layer, second_layer, spindle_layer;
+Layer *dial_layer, *hour_layer, *minute_layer, *second_layer, *spindle_layer;
 GPoint centre, v_centre;
-GPath hour_hand, minute_hand, spindle;
-GFont face_font;
+GPath *hour_hand, *minute_hand, *spindle;
+GFont *face_font;
 GPoint second_points[60][2]; // Cache for points for second hand elements.
 float hand_angles[360]; // Cache for minute and hour hand angles.
 
@@ -97,7 +78,7 @@ void get_point_at_angle(GPoint *target, float angle, int8_t length) {
 
 void dial_layer_update(Layer *me, GContext *ctx) {
   graphics_context_set_fill_color(ctx, BACKGROUND);
-  graphics_fill_rect(ctx, dial_layer.bounds, 0, GCornerNone);
+  graphics_fill_rect(ctx, layer_get_bounds(dial_layer), 0, GCornerNone);
 
   // Draw some dial markings
   graphics_context_set_fill_color(ctx, FOREGROUND);
@@ -113,108 +94,108 @@ void dial_layer_update(Layer *me, GContext *ctx) {
   // The 12 is separated so we can drop half of it.
   GPoint num_point;
   get_point_at_angle(&num_point, get_angle(12, 0), DIAL_RADIUS);
-  graphics_text_draw(ctx, "1", face_font,
+  graphics_draw_text(ctx, "1", face_font,
 		     GRect(num_point.x-16, num_point.y-27, 20, 40),
 		     GTextOverflowModeTrailingEllipsis,
 		     GTextAlignmentCenter, NULL);
-  graphics_text_draw(ctx, "2", face_font,
+  graphics_draw_text(ctx, "2", face_font,
 		     GRect(num_point.x-4, num_point.y-23, 20, 40),
 		     GTextOverflowModeTrailingEllipsis,
 		     GTextAlignmentCenter, NULL);
   // The rest are a bit simpler.
   get_point_at_angle(&num_point, get_angle(12, 3), DIAL_RADIUS);
-  graphics_text_draw(ctx, "3", face_font,
+  graphics_draw_text(ctx, "3", face_font,
 		     GRect(num_point.x-10, num_point.y-28, 20, 40),
 		     GTextOverflowModeTrailingEllipsis,
 		     GTextAlignmentCenter, NULL);
   get_point_at_angle(&num_point, get_angle(12, 6), DIAL_RADIUS);
-  graphics_text_draw(ctx, "6", face_font,
+  graphics_draw_text(ctx, "6", face_font,
 		     GRect(num_point.x-8, num_point.y-24, 20, 40),
 		     GTextOverflowModeTrailingEllipsis,
 		     GTextAlignmentCenter, NULL);
   get_point_at_angle(&num_point, get_angle(12, 9), DIAL_RADIUS);
-  graphics_text_draw(ctx, "9", face_font,
+  graphics_draw_text(ctx, "9", face_font,
 		     GRect(num_point.x-8, num_point.y-28, 20, 40),
 		     GTextOverflowModeTrailingEllipsis,
 		     GTextAlignmentCenter, NULL);
 }
 
 void second_layer_update(Layer *me, GContext *ctx) {
-  PblTm now;
-  get_time(&now);
+  time_t t = time(NULL);
+  struct tm *now = localtime(&t);
 
-  if (second_points[now.tm_sec][0].x == 0) { // points have not been cached
-    float angle = get_angle(60, now.tm_sec);
-    GPoint sec;
+  if (second_points[now->tm_sec][0].x == 0) { // points have not been cached
+    float angle = get_angle(60, now->tm_sec);
     // end of second hand
-    get_point_at_angle(&second_points[now.tm_sec][0], angle, DIAL_RADIUS-8);
+    get_point_at_angle(&second_points[now->tm_sec][0], angle, DIAL_RADIUS-8);
     // centre of circle on second hand
-    get_point_at_angle(&second_points[now.tm_sec][1], angle, DIAL_RADIUS-18);
+    get_point_at_angle(&second_points[now->tm_sec][1], angle, DIAL_RADIUS-18);
   }
   graphics_context_set_stroke_color(ctx, FOREGROUND);
   graphics_context_set_fill_color(ctx, FOREGROUND);
-  graphics_draw_line(ctx, centre, second_points[now.tm_sec][0]);
-  graphics_fill_circle(ctx, second_points[now.tm_sec][1], 7);
+  graphics_draw_line(ctx, centre, second_points[now->tm_sec][0]);
+  graphics_fill_circle(ctx, second_points[now->tm_sec][1], 7);
 }
 
 void minute_layer_update(Layer *me, GContext *ctx) {
-  PblTm now;
-  get_time(&now);
+  time_t t = time(NULL);
+  struct tm *now = localtime(&t);
 
   // Want to update every ten seconds; 6 events per minute.
-  int16_t offset = now.tm_min*6 + (now.tm_sec / 10);
+  int16_t offset = now->tm_min*6 + (now->tm_sec / 10);
   if (hand_angles[offset] == 10) { // angle has not been cached
     hand_angles[offset] = get_angle(360, offset);
   }
-  gpath_rotate_to(&minute_hand, hand_angles[offset]);
+  gpath_rotate_to(minute_hand, hand_angles[offset]);
 
   graphics_context_set_fill_color(ctx, FOREGROUND);
   graphics_context_set_stroke_color(ctx, BACKGROUND);
-  gpath_draw_filled(ctx, &minute_hand);
-  gpath_draw_outline(ctx, &minute_hand);
+  gpath_draw_filled(ctx, minute_hand);
+  gpath_draw_outline(ctx, minute_hand);
 }
 
 void hour_layer_update(Layer *me, GContext *ctx) {
-  PblTm now;
-  get_time(&now);
+  time_t t = time(NULL);
+  struct tm *now = localtime(&t);
 
-  int16_t offset = (now.tm_hour%12)*30 + (now.tm_min / 2);
+  int16_t offset = (now->tm_hour%12)*30 + (now->tm_min / 2);
   if (hand_angles[offset] == 10) { // angle has not been cached
     hand_angles[offset] = get_angle(360, offset);
   }
-  gpath_rotate_to(&hour_hand, hand_angles[offset]);
+  gpath_rotate_to(hour_hand, hand_angles[offset]);
 
   graphics_context_set_fill_color(ctx, FOREGROUND);
   graphics_context_set_stroke_color(ctx, BACKGROUND);
-  gpath_draw_filled(ctx, &hour_hand);
-  gpath_draw_outline(ctx, &hour_hand);
+  gpath_draw_filled(ctx, hour_hand);
+  gpath_draw_outline(ctx, hour_hand);
 }
 
 void spindle_layer_update(Layer *me, GContext *ctx) {
     graphics_context_set_fill_color(ctx, FOREGROUND);
     graphics_context_set_stroke_color(ctx, BACKGROUND);
-    gpath_draw_filled(ctx, &spindle);
-    gpath_draw_outline(ctx, &spindle);
+    gpath_draw_filled(ctx, spindle);
+    gpath_draw_outline(ctx, spindle);
 
     graphics_draw_circle(ctx, centre, 3);
 }
 
-void handle_tick(AppContextRef ctx, PebbleTickEvent *t) {
-  layer_mark_dirty(&second_layer);
-  if (t->tick_time->tm_sec % 10 == 0) {
-    layer_mark_dirty(&minute_layer);
+void handle_tick(struct tm *t, TimeUnits units_changed) {
+  layer_mark_dirty(second_layer);
+  if (t->tm_sec % 10 == 0) {
+    layer_mark_dirty(minute_layer);
   }
 }
 
-void handle_init(AppContextRef ctx) {
-  resource_init_current_app(&APP_RESOURCES);
-  window_init(&window, "Gravity");
-  window_stack_push(&window, true /* Animated */);
+void init() {
+  root_window = window_create();
+  window_stack_push(root_window, true /* Animated */);
 
   face_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_STANISLAV_36));
 
   // Initialise centre points and hand caches
-  centre = grect_center_point(&window.layer.frame);
+  Layer *root_window_layer = window_get_root_layer(root_window);
+  GRect root_window_bounds = layer_get_bounds(root_window_layer);
+  centre = grect_center_point(&root_window_bounds);
   v_centre = GPoint(centre.x, (centre.y-V_OFFSET));
   for (int i=0; i<60; i++) {
     second_points[i][0].x = 0;
@@ -227,39 +208,47 @@ void handle_init(AppContextRef ctx) {
   }
 
   // Initialise layers
-  layer_init(&dial_layer, window.layer.bounds);
-  dial_layer.update_proc = dial_layer_update;
-  layer_init(&second_layer, window.layer.bounds);
-  second_layer.update_proc = second_layer_update;
-  layer_init(&minute_layer, window.layer.bounds);
-  minute_layer.update_proc = minute_layer_update;
-  layer_init(&hour_layer, window.layer.bounds);
-  hour_layer.update_proc = hour_layer_update;
-  layer_add_child(&window.layer, &dial_layer);
-  layer_add_child(&window.layer, &second_layer);
-  layer_add_child(&window.layer, &minute_layer);
-  layer_add_child(&window.layer, &hour_layer);
-  layer_init(&spindle_layer, window.layer.bounds);
-  spindle_layer.update_proc = spindle_layer_update;
-  layer_add_child(&window.layer, &spindle_layer);
+  dial_layer = layer_create(root_window_bounds);
+  layer_set_update_proc(dial_layer, dial_layer_update);
+  second_layer = layer_create(root_window_bounds);
+  layer_set_update_proc(second_layer, second_layer_update);
+  minute_layer = layer_create(root_window_bounds);
+  layer_set_update_proc(minute_layer, minute_layer_update);
+  hour_layer = layer_create(root_window_bounds);
+  layer_set_update_proc(hour_layer, hour_layer_update);
+  spindle_layer = layer_create(root_window_bounds);
+  layer_set_update_proc(spindle_layer, spindle_layer_update);
+  layer_add_child(root_window_layer, dial_layer);
+  layer_add_child(root_window_layer, second_layer);
+  layer_add_child(root_window_layer, minute_layer);
+  layer_add_child(root_window_layer, hour_layer);
+  layer_add_child(root_window_layer, spindle_layer);
 
   // Initialise hands
-  gpath_init(&minute_hand, &MINUTE_HAND_PATH_POINTS);
-  gpath_move_to(&minute_hand, centre);
-  gpath_init(&hour_hand, &HOUR_HAND_PATH_POINTS);
-  gpath_move_to(&hour_hand, centre);
-  gpath_init(&spindle, &BOLT_PATH_POINTS);
-  gpath_move_to(&spindle, centre);
+  minute_hand = gpath_create(&MINUTE_HAND_PATH_POINTS);
+  gpath_move_to(minute_hand, centre);
+  hour_hand = gpath_create(&HOUR_HAND_PATH_POINTS);
+  gpath_move_to(hour_hand, centre);
+  spindle = gpath_create(&BOLT_PATH_POINTS);
+  gpath_move_to(spindle, centre);
+
+  tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
 }
 
+void deinit(void) {
+  gpath_destroy(spindle);
+  gpath_destroy(hour_hand);
+  gpath_destroy(minute_hand);
+  layer_destroy(spindle_layer);
+  layer_destroy(hour_layer);
+  layer_destroy(minute_layer);
+  layer_destroy(second_layer);
+  layer_destroy(dial_layer);
+  window_destroy(root_window);
+}
 
-void pbl_main(void *params) {
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init,
-    .tick_info = {
-      .tick_handler = handle_tick,
-      .tick_units = SECOND_UNIT
-    }
-  };
-  app_event_loop(params, &handlers);
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
 }
